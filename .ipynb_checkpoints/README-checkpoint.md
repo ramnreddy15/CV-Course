22cb4ecff18d0bb75d2ac475a073ce06f2a47ae8
Ram Reddy <ram.n.reddy15@gmail.com>
2021-08-24 18:58:25 -0400

    Compiler works

--- .ipynb_checkpoints/README-checkpoint.md
+++
@@ -1,2 +0,0 @@
-# CV-Course
-A computer vision course I took at TJ from 2021-2022
--- TestC++/t.cpp
+++ TestC++/t.cpp
@@ -3,6 +3,9 @@ using namespace std;
 
 int main()
 {
+    int x;
     cout << "\n\nHello World,\nWelcome to my first C ++ program on Ubuntu Linux\n\n" << endl;
+    cin >> x;
+    cout << x;
     return 0;
 }
