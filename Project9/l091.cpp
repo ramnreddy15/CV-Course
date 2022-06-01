55ce883669e66e24f894e15a49688670c93a294f
Ram Reddy <ram.n.reddy15@gmail.com>
2022-06-01 03:39:05 +0000

    Done

--- Project9/l091.cpp
+++ Project9/l091.cpp
@@ -25,10 +25,10 @@ using namespace cv;
 void part1() {
     Mat frame, prev, flow, temp;
     VideoCapture capture;
-    vector<vector<Point3f> > objpoints;
+    vector<vector<Point3f> > objpoints, objpointsAll;
     vector<Point2f> projectionPoints;
     vector<Point3f> cubePoints;
-    vector<vector<Point2f> > imgpoints;
+    vector<vector<Point2f> > imgpoints, imgpointsAll;
     std::vector<cv::Point3f> objp;
     Size patternsize(7,7); //interior number of corners
     
@@ -42,10 +42,10 @@ void part1() {
     // cubePoints.push_back(Point3f(2, 0, 0));
     // cubePoints.push_back(Point3f(0, 0, 0));
     // cubePoints.push_back(Point3f(0, 2, 0));
-    cubePoints.push_back(Point3f(1, 1, 2));
-    cubePoints.push_back(Point3f(1, -1, 2));
-    cubePoints.push_back(Point3f(-1, -1, 2));
-    cubePoints.push_back(Point3f(-1, 1, 2));
+    cubePoints.push_back(Point3f(1, 1, -2));
+    cubePoints.push_back(Point3f(1, -1, -2));
+    cubePoints.push_back(Point3f(-1, -1, -2));
+    cubePoints.push_back(Point3f(-1, 1, -2));
     cubePoints.push_back(Point3f(1, 1, 0));
     cubePoints.push_back(Point3f(1, -1, 0));
     cubePoints.push_back(Point3f(-1, -1, 0));
@@ -66,9 +66,10 @@ void part1() {
     vector<Point2f> corners, cubeCorners;
     capture.open("withChessBoard.MOV");
     capture.read(frame);
+    int count = 0;
     VideoWriter output("vr.avi", cv::VideoWriter::fourcc('M', 'J', 'P', 'G'), capture.get(CAP_PROP_FPS), cv::Size(frame.cols, frame.rows));
     while(!frame.empty()) {
-        rotate(frame,frame,ROTATE_180);
+        // rotate(frame,frame,ROTATE_180);
  //source image
         cvtColor(frame, gray, CV_BGR2GRAY);
         vector<Point2f> newCorners = cubeCorners;
@@ -87,14 +88,19 @@ void part1() {
                 for (Point2f p : corners) {
                     circle(frame, p,10,(255,0,0));
                 }
-                objpoints.push_back(objp);
                 vector<Point2f> temp;
                 temp.push_back(corners[18]);
                 temp.push_back(corners[16]);
                 temp.push_back(corners[24]);
                 temp.push_back(corners[30]);
                 temp.push_back(corners[32]);
-                imgpoints.push_back(temp);
+                objpointsAll.push_back(objp);
+                imgpointsAll.push_back(temp);
+                if(count%5 == 0) {
+                    objpoints.push_back(objp);
+                    imgpoints.push_back(temp);                    
+                }
+
             
                 cubeCorners.clear();
                 cubeCorners.push_back(corners[18]);
@@ -113,58 +119,67 @@ void part1() {
             TermCriteria criteria = TermCriteria((TermCriteria::COUNT) + (TermCriteria::EPS), 10, 0.03);
             calcOpticalFlowPyrLK(prev, gray, cubeCorners, newCorners, status, err, Size(15,15), 2, criteria);
             cubeCorners = newCorners;
-            objpoints.push_back(objp);
             vector<Point2f> temp;
             temp.push_back(cubeCorners[0]);
             temp.push_back(cubeCorners[1]);
             temp.push_back(cubeCorners[2]);
             temp.push_back(cubeCorners[3]);
             temp.push_back(cubeCorners[4]);
-            imgpoints.push_back(temp);
+            objpointsAll.push_back(objp);
+            imgpointsAll.push_back(temp);   
+            if(count %5 == 0) {
+                objpoints.push_back(objp);
+                imgpoints.push_back(temp);                
+            }
+
             for (Point2f p : cubeCorners) {
                 circle(frame, p,10,(255,0,0));
             }
         }
+        count++;
         output.write(frame);
-
-
-        
-//         number++;
-//         if (number >= 100) {
-//             Mat cameraMatrix,distCoeffs,R,T,R1,P;
-//             calibrateCamera(objpoints, imgpoints, cv::Size(gray.rows,gray.cols), cameraMatrix, distCoeffs, R, T);
-//             std::cout << "cameraMatrix : " << cameraMatrix << std::endl;
-//             std::cout << "distCoeffs : " << distCoeffs << std::endl;
-//             std::cout << "Rotation vector : " << R << std::endl;
-//             std::cout << "Translation vector : " << T << std::endl;
-//             solvePnP(objpoints[0], imgpoints[0], cameraMatrix, distCoeffs, R,T);
-// //             Rodrigues(R,R1);
-// //             P = cameraMatrix.dot()
-//             projectPoints(cubePoints,R,T,cameraMatrix,distCoeffs,projectionPoints);
-//             cout << projectionPoints << endl;
-//             for(Point2f p: projectionPoints) {
-                
-//                 circle(frame, p,10,(0,0,255));
-//             }
-//             //     drawChessboardCorners(frame, patternsize, Mat(corners), patternfound);
-//             imwrite("tesst.jpg", frame);
-//             break;
-//         }
         
         gray.copyTo(prev);
         capture.read(frame);
 
     }
-    cout << "here" << endl;
-
-    Mat cameraMatrix,distCoeffs,R,T,R1,P;
+    cout << objpoints.size() << endl;
+    capture.release();
+    Mat cameraMatrix,distCoeffs,R,T;
     calibrateCamera(objpoints, imgpoints, cv::Size(gray.rows,gray.cols), cameraMatrix, distCoeffs, R, T);
     std::cout << "cameraMatrix : " << cameraMatrix << std::endl;
     std::cout << "distCoeffs : " << distCoeffs << std::endl;
-//     std::cout << "Rotation vector : " << R << std::endl;
-//     std::cout << "Translation vector : " << T << std::endl;
+    capture.open("withChessBoard.MOV");
+    vector<Point2i> lines;
+    for(int i =0; i<cubePoints.size(); i++) {
+    for(int j=i+1; j<cubePoints.size(); j++) {
+        int y = cubePoints[i].y;
+        int z = cubePoints[i].z;
+        int x1 = cubePoints[j].x;
+        int y1 = cubePoints[j].y;
+        int z1 = cubePoints[j].z;
+        int x = cubePoints[i].x;
+        if(sqrt(pow(x1-x,2)+pow(y1-y,2)+pow(z1-z,2)) == 2) {
+            lines.push_back(Point2i(i,j));
+        }
+    }
+}
+    count = 0;
+    capture.read(frame);
+    while(!frame.empty()) {
+        solvePnP(objpointsAll[count], imgpointsAll[count], cameraMatrix, distCoeffs, R,T);
+        projectPoints(cubePoints,R,T,cameraMatrix,distCoeffs,projectionPoints);
+        for(Point2f p: projectionPoints) {           
+            circle(frame, p,10,(0,255,2505));
+        }    
+        for(Point2i l:lines) {
+            line(frame, projectionPoints[l.x], projectionPoints[l.y], Scalar(255,0,0),5);
+        }
+        count++;
+        output.write(frame);
+        capture.read(frame);
+    }
     
-    capture.release();
     output.release();
 
 }
